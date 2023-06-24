006352d992da31bb2e396d651ed776150c6f02d8
allancassio <allan.cassio1@gmail.com>
2023-06-24 20:52:29 -0300

    created login route for email access

--- src/routes/recipesRoutes.ts
+++ src/routes/recipesRoutes.ts
@@ -1,6 +1,51 @@
 import { FastifyInstance } from "fastify";
-import { recipesController } from "../controllers/recipesController";
+import { checkSessionIdExists } from '../middlewares/checkSessionIdExists'
+import { knexDb } from "../dbConfig";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
 export async function recipesRoutes (app: FastifyInstance)  {
-  app.get('/', recipesController.create)
+  app.post('/create', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const createRecipesBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      diet_conform: z.boolean()
+    })
+
+    const {
+      name, description, diet_conform
+    } = createRecipesBodySchema.parse(request.body)
+
+    const {sessionId} = request.cookies
+    const userBySessioId = await knexDb('users')
+      .where('session_id', sessionId)
+      .first()
+
+    if(!userBySessioId) return reply.status(401).send("não é o usuário")
+
+    await knexDb('recipes').insert({
+      id: randomUUID(),
+      name,
+      description,
+      diet_conform,
+      user_id: userBySessioId.id
+    })
+    
+    return reply.status(201).send(userBySessioId)
+  })
+
+  //TODO: essa rota é para listar todos as refeições do usuário. o id é do usuário
+  app.get('/:id', async (request, reply) => {
+    const idParams = z.object({
+      id: z.string(),
+    })
+
+    const { id } = idParams.parse(request.params)
+    
+    const a = await knexDb('recipes').where('user_id', id).select('*')
+
+    return reply.status(200).send(a)
+  })
 }
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
-import { checkSessionIdExists } from '../middlewares/checkSessionIdExists'
 import { randomUUID } from "node:crypto";
 import { knexDb } from "../dbConfig";
+import { checkSessionIdExists } from "../middlewares/checkSessionIdExists";
 
 export async function usersRoutes (app: FastifyInstance)  {
   app.post('/create', async (request, reply) => {
@@ -21,13 +21,48 @@ export async function usersRoutes (app: FastifyInstance)  {
       })
     }
 
-    await knexDb('users').insert({
+    const session_id = await knexDb('users').insert({
       id: randomUUID(),
       name,
       email,
-      
+      session_id: sessionId
+    }).returning('session_id')
+
+    return reply.status(201).send(session_id)
+  })
+
+  app.post('/login', async (request, reply) => {
+    const loginUserSchema = z.object({
+      email: z.string()
     })
 
-    return reply.status(201).send('created')
+    const { email } = loginUserSchema.parse(request.body)
+
+    const user = await knexDb('users').where('email', email).first()
+
+    if(!user) return reply.status(404).send("There aren't user registred")
+
+    const {sessionId} = request.cookies
+
+    if(sessionId && sessionId === user.session_id){
+      return reply.status(200).send("allowed access")
+    }
+    const newSessionId = randomUUID()
+    reply.cookie('sessionId', newSessionId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+    })
+
+    await knexDb("users").where('id', user.id).update({
+      session_id: newSessionId
+    })
+
+    return reply.status(200).send("allowed access")
+  })
+
+  //TODO: esta rota é so para retornar os usuários do banco de dados. apenas para teste
+  app.get('/all', async (request, reply) => {
+    const users = await knexDb('users').select('*')
+    return reply.status(200).send({users})
   })
 }
